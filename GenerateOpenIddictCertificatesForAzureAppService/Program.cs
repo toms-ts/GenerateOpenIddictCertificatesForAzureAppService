8b4faf745df6db80815e847f4123ec43a456c117
toms-ts <68369387+toms-ts@users.noreply.github.com>
2024-09-16 21:17:39 +1000

    Add project files.

---
+++ GenerateOpenIddictCertificatesForAzureAppService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+
+using var algorithm = RSA.Create(keySizeInBits: 2048);
+
+var subject = new X500DistinguishedName("CN=Fabrikam Server Encryption Certificate");
+var request = new CertificateRequest(subject, algorithm, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+request.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.KeyEncipherment, critical: true));
+
+var certificate = request.CreateSelfSigned(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddYears(2));
+
+File.WriteAllBytes("server-encryption-certificate.pfx", certificate.Export(X509ContentType.Pfx, "YOUR_PASSWORD"));
